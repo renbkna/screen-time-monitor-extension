01a148c0eb3f1649f20395974d666efa14a30b4a
ren <92011413+renbkna@users.noreply.github.com>
2024-12-14 02:07:05 +0100

    Update Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,18 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFiles: ['./tests/setup.js'],
+  setupFilesAfterEnv: ['jest-extended'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   collectCoverageFrom: [
     'src/**/*.{js,jsx}',
     '!src/index.js',
     '!src/setupTests.js',
   ],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
   },
+  verbose: true
 };
